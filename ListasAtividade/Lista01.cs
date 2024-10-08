7c92128d7fdbdf96a31b7cd644c29f074bae6fc0
ISABELLE DA SILVA MARTINS <isabelle.smartins3@senacsp.edu.br>
2024-10-07 22:17:33 -0300

    primeiro commit

---
+++ ListasAtividade/Lista01.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasAtividade
+{
+    internal class Lista01
+
+    {
+
+        public void Dobro()
+        {
+            
+
+        }
+
+
+        public void AntecessorSucessor()
+        {
+            int i,cal1,cal2;
+            Console.WriteLine("Digite um número: ");
+            i= int.Parse(Console.ReadLine());   
+            cal1= i-1;
+            cal2= i +1;
+            Console.WriteLine("O antecessor de " + i + " é: " + cal1 + " e o sucessor é: " + cal2);
+
+
+        }
+
+        public void Media()
+        {
+            double n1, n2, soma;
+            Console.WriteLine("Digite a primeira nota do aluno: ");
+            n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a segunda nota:");
+            n2 = double.Parse(Console.ReadLine());
+            soma=n1 + n2;
+            Console.WriteLine("A Média do aluno é:" + soma / 2);
+
+
+        }
+
+        public void SomaInteiros()
+        {
+
+
+            int valor1, valor2, soma;
+            Console.WriteLine("Digite o primeiro número: ");
+            valor1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            valor2 = int.Parse(Console.ReadLine());
+            soma = valor1 + valor2;
+            Console.WriteLine("A soma dos números é: " + soma);
+
+
+
+        }
+
+        public void Salario()
+        {
+            string nome;
+            double valor;
+
+            Console.WriteLine("Qual o seu nome? ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Atualmente quanto é o seu salário? ");
+            valor = double.Parse(Console.ReadLine());
+            Console.WriteLine(nome + ", atualmente possui o salário no valor de R$ " + valor);
+
+            Console.ReadKey();
+
+        }
+
+        public void BoasVindas()
+        {
+            string nome;
+            Console.WriteLine("Qual o seu nome?");
+            nome = Console.ReadLine();
+            Console.WriteLine("Olá, " + nome + ", é um prazer te conhecer!!");
+
+            Console.ReadKey();
+
+        }
+
+        public void Mundo()
+        {
+            Console.WriteLine("Olá, Mundo!! :) ");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ListasAtividade/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasAtividade
+{
+    internal class Program
+    {
+
+
+
+        // Ctrl + K e Ctrl+D arruma o codigo selecionado.
+        //CTRL+K e Ctrl+C deixa em comentário
+        // Console.Clear();limpa a tela
+        //Console.ReadKey(); dar pause no programa para a pessoa ler. congela a tela
+        // menu com switch case
+
+        static void Main(string[] args)
+        {
+
+
+
+            string opcao;
+            Lista01 lista01 = new Lista01();
+            Lista02 lista02 = new Lista02();
+            Lista03 lista03 = new Lista03();
+            Lista04 lista04 = new Lista04();
+
+
+            //Criando um manual com switch case
+            do
+            {
+
+                Console.WriteLine("Escolha uma operação:\n");
+                Console.WriteLine("Lista 01 ");
+                Console.WriteLine("1 - Olá, Mundo!");
+                Console.WriteLine("2 - Boas vindas");
+                Console.WriteLine("3 - Qual o seu salário? ");
+                Console.WriteLine("4 - Soma de inteiros");
+                Console.WriteLine("5 - Média das notas");
+                Console.WriteLine("6 - Antecessor e Sucessor");
+
+
+
+
+
+                int escolha = Convert.ToInt32(Console.ReadLine());
+
+                switch (escolha)
+                {
+                    case 1:
+                        lista01.Mundo();
+                        break;
+                    case 2:
+                        lista01.BoasVindas();
+                        break;
+                    case 3:
+                        lista01.Salario();
+                        break;
+                    case 4:
+                        lista01.SomaInteiros();
+                        break;
+                    case 5:
+                        lista01.Media();
+                        break;
+                        case 6:
+                      lista01.AntecessorSucessor(); 
+                        break;
+                                                
+
+                    default:
+                        Console.WriteLine("Operação inválida.");
+                        break;
+                }
+                Console.WriteLine("Deseja continuar no programa (S/N)");
+                opcao = Console.ReadLine().ToUpper(); // to upper serve para ele ler o que o usuário digita independente de estar maiúsculo ou minúsculo
+
+
+
+
+            } while (opcao == "S");
+        }
+
+       
+
+       
+    }
+}
